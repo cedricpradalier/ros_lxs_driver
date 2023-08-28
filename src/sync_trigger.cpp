3767df4e95b59e041e5ad3fa52609badcb3f5195
Cedric Pradalier <cedric.pradalier@gmail.com>
2023-08-28 14:54:24 +0200

    Added mechanism for trigger divider

--- src/sync_trigger.cpp
+++ src/sync_trigger.cpp
@@ -7,7 +7,9 @@ class SyncTrigger {
     protected:
         int num_publisher;
         double rate;
-        std::vector<ros::Publisher> publishers;
+        int divider;
+        std::vector<ros::Publisher> publishers, publishers_div;
+        std::vector<size_t> div;
         ros::Timer timer;
 
         size_t next;
@@ -16,20 +18,35 @@ class SyncTrigger {
             std_msgs::Header header;
             header.stamp = ros::Time::now();
             publishers[next].publish(header);
+            if (divider > 0) {
+                div[next] -= 1;
+                if (div[next]==0) {
+                    publishers_div[next].publish(header);
+                    div[next] = divider;
+                }
+            }
+
             next = (next + 1) % publishers.size();
         }
 
     public:
         SyncTrigger(ros::NodeHandle & nh) {
             next = 0;
+            divider = -1;
             nh.param("num_publisher",num_publisher,2);
             nh.param("rate",rate,100.0);
+            nh.param("divider",divider,-1);
             num_publisher = std::max(1,num_publisher);
 
             for (int i=0;i<num_publisher;i++) {
                 char name[128];
                 sprintf(name,"trigger%d",i);
                 publishers.push_back(nh.advertise<std_msgs::Header>(name,1));
+                if (divider > 0) {
+                    sprintf(name,"trigger%d_div",i);
+                    publishers_div.push_back(nh.advertise<std_msgs::Header>(name,1));
+                    div.push_back(divider);
+                }
             }
             timer = nh.createTimer(ros::Duration(1./rate), &SyncTrigger::triggerCallback,this);
         }
